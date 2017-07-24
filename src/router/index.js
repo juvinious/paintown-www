3d166706322e5e17ead3424a2b661ca9df5a3bfc
juvinious <juvinious@gmail.com>
2017-07-24 19:47:05 -0400

    Add bootstrap and font awesome components.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import BootstrapVue from 'bootstrap-vue'
+import Icon from 'vue-awesome/components/Icon'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// only import the icons you use to reduce bundle size
+// import 'vue-awesome/icons/flag'
+
+// or import all icons if you don't care about bundle size
+import 'vue-awesome/icons'
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
+Vue.component('icon', Icon)
 
 export default new Router({
   routes: [
